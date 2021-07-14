d4f304cbacbf016a603b97a8ce7513ff696a7db6
alisher <batyr_alisher@mail.ru>
2021-07-14 16:09:38 +0600

    Initial Commit

---
+++ TableViewCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewCode
+//
+//  Created by Алишер Батыр on 14.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
